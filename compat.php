794caa908f4cedaad2228e1a6acb5c1034ca769f
Ryan McCue <me@ryanmccue.info>
2011-12-24 02:12:34 +1000

    Don't allow HTML in short descriptions

--- compat.php
+++ compat.php
@@ -38,6 +38,7 @@ class _Automattic_Readme extends Baikonur_ReadmeParser {
 			$result->remaining_content = '';
 		}
 
+		$result->short_description = self::sanitize_text($result->short_description);
 		$result->donate_link = esc_url($result->donate_link);
 
 		$result->requires_at_least = $result->requires;
