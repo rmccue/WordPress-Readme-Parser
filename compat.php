2f7be9f60775423d785cd890742097a74c5169f9
Ryan McCue <me@ryanmccue.info>
2011-12-24 01:27:05 +1000

    Rework the compat class

--- compat.php
+++ compat.php
@@ -7,13 +7,78 @@ if ( !defined('WORDPRESS_README_MARKDOWN') ) {
 		define('WORDPRESS_README_MARKDOWN', dirname(__FILE__) . '/markdown.php');
 }
 
-if (!class_exists('Markdown')) {
-	require_once(WORDPRESS_README_MARKDOWN);
-}
+require_once(dirname(__FILE__) . '/ReadmeParser.php');
 
-class Automattic_Readme extends Baikonur_ReadmeParser {
+class _Automattic_Readme extends Baikonur_ReadmeParser {
+	public static function parse_readme($file) {
+		$contents = file($file);
+		return self::parse_readme_contents($contents);
+	}
+
 	public static function parse_readme_contents($contents) {
+		if (empty($contents)) {
+			return array();
+		}
+
 		$result = parent::parse_readme_contents($contents);
-		return (array) $result;
+		foreach ($result->sections as &$section) {
+			$section = self::filter_text($section);
+		}
+		if (!empty($result->upgrade_notice)) {
+			foreach ($result->upgrade_notice as &$notice) {
+				$notice = self::sanitize_text($notice);
+			}
+		}
+
+		if (!empty($result->remaining_content)) {
+			$result->remaining_content = implode("\n", $result->remaining_content);
+			$result->remaining_content = str_replace("</h3>\n\n", "</h3>\n", $result->remaining_content);
+		}
+		else {
+			$result->remaining_content = '';
+		}
+
+		$result->requires_at_least = $result->requires;
+		$result->tested_up_to = $result->tested;
+		unset($result->requires, $result->tested);
+		$result = ((array) $result);
+		return $result;
+	}
+
+	protected static function sanitize_text( $text ) { // not fancy
+		$text = strip_tags($text);
+		$text = esc_html($text);
+		$text = trim($text);
+		return $text;
+	}
+
+	protected static function filter_text( $text ) {
+		$text = trim($text);
+		//$text = self::code_trick($text); // A better parser than Markdown's for: backticks -> CODE
+
+		$allowed = array(
+			'a' => array(
+				'href' => array(),
+				'title' => array(),
+				'rel' => array()),
+			'blockquote' => array('cite' => array()),
+			'br' => array(),
+			'p' => array(),
+			'code' => array(),
+			'pre' => array(),
+			'em' => array(),
+			'strong' => array(),
+			'ul' => array(),
+			'ol' => array(),
+			'li' => array(),
+			'h3' => array(),
+			'h4' => array()
+		);
+
+		$text = balanceTags($text);
+
+		$text = wp_kses( $text, $allowed );
+		$text = trim($text);
+		return $text;
 	}
 }
